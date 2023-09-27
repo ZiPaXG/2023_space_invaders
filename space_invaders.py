131015540d50eabeab05c7ce534ba0498938b579
ZiPaXG <shepelev.danilka@bk.ru>
2023-09-27 15:05:20 +0300

    добавлен выстрел по нажатию ЛКМ

--- space_invaders.py
+++ space_invaders.py
@@ -38,8 +38,17 @@ player_dx = 0
 player_x = screen_width/2 - playerWidth/2
 player_y = screen_height - playerHeight - player_gap
 
+# Пуля
+bulletImg = pg.image.load('src/bullet.png')
+bulletWidth, bulletHeight = bulletImg.get_size()
+bullet_x = player_x + bulletWidth / 2
+bullet_y = player_y - bulletHeight
+bullet_dy = -5
+bullet_isVisible = False
+
 def model_update():
     player_model()
+    bullet_model()
 def player_model():
     global  player_x
     player_x += player_dx
@@ -47,9 +56,22 @@ def player_model():
         player_x = 0
     if player_x > screen_width - playerWidth:
         player_x = screen_width - playerWidth
+def bullet_model():
+    global bullet_y, bullet_isVisible
+    bullet_y += bullet_dy
+    if bullet_y < 0:
+        bullet_isVisible = False
+def bullet_create():
+    global bullet_x, bullet_y, bullet_isVisible
+    bullet_x = player_x + bulletWidth / 2
+    bullet_y = player_y - bulletHeight
+    bullet_isVisible = True
+
 def display_redraw():
     display.blit(bg_img, (0, 0))
     display.blit(playerImg, (player_x, player_y))
+    if bullet_isVisible:
+        display.blit(bulletImg, (bullet_x, bullet_y))
     pg.display.update()
 
 def event_processing():
@@ -70,6 +92,12 @@ def event_processing():
                 player_dx = player_velocity
         if event.type == pg.KEYUP:
             player_dx = 0
+        if event.type == pg.MOUSEBUTTONDOWN:
+            key = pg.mouse.get_pressed()
+            print(f'{key[0]}')
+            if not bullet_isVisible:
+                bullet_create()
+
     clock.tick(FPS)
     return isRunning
 
